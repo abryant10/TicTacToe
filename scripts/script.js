7458c3d9c2f1b3f9fa239641e06c90fd7aa8664c
Alex Bryant <alexanderpbryant@gmail.com>
2021-04-29 16:59:18 -0500

    bassic css added

---
+++ scripts/script.js
@@ -0,0 +1,37 @@
+// gamebord object with gameboard array inside -module
+
+//players object -factory function
+
+//object that controls game - module
+
+/*
+write a JavaScript function that will render 
+the contents of the gameboard array to the webpage 
+(for now you can just manually fill in the array with "X"s and "O"s)
+*/
+
+/*
+Build the functions that allow players to add marks to a 
+specific spot on the board, and then tie it to the DOM, 
+letting players click on the gameboard to place their marker. 
+Don’t forget the logic that keeps players from playing in spots that are
+ already taken!
+Think carefully about where each bit of logic should reside. 
+Each little piece of functionality should be able to fit in the game, 
+player or gameboard objects.. but take care to put them in “logical” places. 
+Spending a little time brainstorming here can make your life much easier 
+later!
+*/
+
+// Build the logic that checks for when the game is over! 
+// Should check for 3-in-a-row and a tie.
+
+
+// Clean up the interface to allow players to put in their names, 
+// include a button to start/restart the game and add a display 
+// element that congratulates the winning player!
+
+// Optional - If you’re feeling ambitious create an AI so that a player can play against the computer!
+// Start by just getting the computer to make a random legal move.
+// Once you’ve gotten that, work on making the computer smart. It is possible to create an unbeatable AI using the minimax algorithm (read about it here, some googling will help you out with this one)
+// If you get this running definitely come show it off in the chatroom. It’s quite an accomplishment!
